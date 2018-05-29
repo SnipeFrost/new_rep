561dd6aa3d0ec7b8679666ccec9a36cd53e0ef56
SnipeFrost <36679488+SnipeFrost@users.noreply.github.com>
2018-05-30 00:34:53 +0300

    Update class.cs

--- class.cs
+++ class.cs
@@ -1,27 +1,27 @@
-class Registration_data { // містить реєстраційні дані нового користувача 
+class Registration_data { 
   constructor() {
     this.name = ""; this.1ast_name = ; this.photo = ; this.emai1 = "";
     this.password_hash = "";
                 }
 }
 
-class Login_data { // міСТИть дані для входу користувача до системи
+class Login_data { // 
   constructor() {
     this.email =""; this.password_hash = "";
                 }
 }
-class Review_data { // інформація про відгук
+class Review_data { 
   constructor() {
     this.page_id = 0; this.rating = e; this.text = "";
                 }
 }
-class Page  // сторінка, якій відповідає відгук
+class Page  
    constructor() {
     this.id = е;
-    this.ur1 = ""; this.tit1e = ""; this.items = []; // містить екземпляри Item
+    this.ur1 = ""; this.tit1e = ""; this.items = []; 
                  }
 }
-class Item  // Рубрика на сторінки, для якої можна додати відгук
+class Item  
 constructor(){
 this.id = е; this.page_id = 0; this.num = 0; this.name = ;}
 }
